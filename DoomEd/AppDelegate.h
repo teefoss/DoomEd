bfb79607ae0fd55d75bde334899a0cccba4d2e39
xiphiasnonus <foster.pianist@gmail.com>
2018-06-29 20:36:20 -0400

    Initial Commit

---
+++ DoomEd/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DoomEd
+//
+//  Created by Thomas Foster on 6/29/18.
+//  Copyright © 2018 Thomas Foster. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
