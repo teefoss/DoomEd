f0d657e9b948c2739ac3c0651fd98bd6bd012530
xiphiasnonus <foster.pianist@gmail.com>
2018-06-29 22:59:17 -0400

    initial commit

---
+++ DoomEd/NXConvert.h
@@ -0,0 +1,26 @@
+//
+//  NXConvert.h
+//  DoomEd
+//
+//  Created by Thomas Foster on 6/29/18.
+//  Copyright © 2018 Thomas Foster. All rights reserved.
+//
+
+#ifndef NXConvert_h
+#define NXConvert_h
+
+#import <Cocoa/Cocoa.h>
+
+#define NXApp 		NSApp
+#define NXPoint 	NSPoint
+#define NX_SOVER 	NSCompositingOperationSourceOver
+
+typedef NSObject Object;
+typedef NSView View;
+typedef NSWindow Window;
+typedef NSRect NXRect;
+
+
+
+
+#endif /* NXConvert_h */
