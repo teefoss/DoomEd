a698811f79660698ded37450ec83aa9922bd8a68
xiphiasnonus <foster.pianist@gmail.com>
2018-07-11 14:13:59 -0400

    texturepatchview updated

--- DoomEd/TexturePatchView.m
+++ DoomEd/TexturePatchView.m
@@ -4,7 +4,9 @@
 #import	"Coordinator.h"
 #import 	"TexturePatchView.h"
 #import	"TextureEdit.h"
+
 #import "Storage.h"
+#import "postscript.h"
 
 @implementation TexturePatchView
 
@@ -13,14 +15,15 @@
 //	Init the storage for the Patch Palette dividers
 //
 //==============================================================
-- initFrame:(const NXRect *)frameRect
+//- initFrame:(const NXRect *)frameRect
+- initWithFrame:(NSRect)frame
 {
 	dividers_i = [	[ Storage alloc ]
 				initCount:		0
 				elementSize:	sizeof (divider_t )
 				description:	NULL ];
 				
-	[super	initFrame:frameRect];
+	[super	initWithFrame:frame];
 	return self;
 }
 
@@ -57,7 +60,8 @@
 //	Draw the Patch Palette in the Texture Editor
 //
 //==============================================================
-- drawSelf:(const NXRect *)rects :(int)rectCount
+//- drawSelf:(const NXRect *)rects :(int)rectCount
+- (void)drawRect:(NSRect)dirtyRect
 {
 	int 		i, max, patchnum,selectedPatch;
 	apatch_t	*patch;
@@ -67,10 +71,13 @@
 	selectedPatch = [textureEdit_i	getCurrentPatch];
 	patchnum = 0;
 	while ((patch = [textureEdit_i	getPatch:patchnum++]) != NULL)
-		if (NXIntersectsRect(&patch->r,&rects[0]))
-			[patch->image		composite:NX_SOVER toPoint:&patch->r.origin];
+//		if (NXIntersectsRect(&patch->r,&rects[0]))
+		if (NXIntersectsRect(&patch->r, &dirtyRect))
+//			[patch->image		composite:NX_SOVER toPoint:&patch->r.origin];
+			[patch->image compositeToPoint:patch->r.origin operation:NX_SOVER];
 	
-	[self	getFrame:&clipview];
+//	[self	getFrame:&clipview];
+	clipview = [self frame];
 	if (selectedPatch >= 0)
 	{
 		patch = [textureEdit_i	getPatch:selectedPatch];
@@ -80,61 +87,67 @@
 		r.size.width += 10;
 		r.size.height += 10;
 		DE_DrawOutline(&r);
-		[patch->image		composite:NX_SOVER toPoint:&patch->r.origin];
+//		[patch->image		composite:NX_SOVER toPoint:&patch->r.origin];
+		[patch->image		compositeToPoint:patch->r.origin operation:NX_SOVER];
 	}
 
 	//
 	//	Draw patch set divider text
 	//
-	PSselectfont("Helvetica-Bold",12);
-	PSrotate ( 0 );
+	
+	// TODO Patch label
+//	PSselectfont("Helvetica-Bold",12);
+//	PSrotate ( 0 );
 	max = [dividers_i	count ];
 	for (i = 0; i < max; i++)
 	{
 		d = [dividers_i	elementAt:i ];
-		PSsetgray ( 0 );
-		PSmoveto( d->x,d->y );
-		PSshow ( d->string );
-		PSstroke ();
+		//PSsetgray ( 0 );
+		//PSmoveto( d->x,d->y );
+		//PSshow ( d->string );		TODO
+		//PSstroke ();
 
 		PSsetrgbcolor ( 148,0,0 );
 		PSmoveto ( d->x, d->y + 12 );
-		PSlineto ( bounds.size.width - SPACING*2, d->y + 12 );
+		PSlineto ( [self bounds].size.width - SPACING*2, d->y + 12 );
 
 		PSmoveto ( d->x, d->y - 2 );
-		PSlineto ( bounds.size.width - SPACING*2, d->y - 2 );
+		PSlineto ( [self bounds].size.width - SPACING*2, d->y - 2 );
 		PSstroke ();
 	}
 	
-	return self;
+	//return self;
 }
 
-- mouseDown:(NXEvent *)theEvent
+//- mouseDown:(NXEvent *)theEvent
+- (void)mouseDown:(NSEvent *)event
 {
 	NXPoint	loc;
 	int		patchnum,selectedPatch;
 	apatch_t *patch;
 	
-	loc = theEvent->location;
-	[self convertPoint:&loc	fromView:NULL];
+//	loc = theEvent->location;
+//	[self convertPoint:&loc	fromView:NULL];
+	loc = [self convertPoint:[event locationInWindow] fromView:nil];
 	
 	selectedPatch = [textureEdit_i	getCurrentPatch];
 	patchnum = 0;
 	while ((patch = [textureEdit_i	getPatch:patchnum++]) != NULL)
-		if ([self	mouse:&loc	inRect:&patch->r] == YES)
+		if ([self	mouse:loc	inRect:patch->r] == YES)
 		{
 			if (selectedPatch != patchnum -1)
 				selectedPatch = patchnum - 1;
 			
-			if (theEvent->data.mouse.click == 2)
+			//if (theEvent->data.mouse.click == 2)
+			if ([event clickCount] == 2)
 				[textureEdit_i	addPatch:selectedPatch];
 				
 			[textureEdit_i	setSelectedPatch:patchnum - 1];
-			[superview	display];
+			[[self superview]	display];
 			break;
 		}
 
-	return self;
+	//return self;
 }
 
 @end
